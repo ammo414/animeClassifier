0e9eb00349a524ced82481a607d9dcd01569d7bd
ammo414 <anmol.eswarapu@protonmail.com>
2024-01-14 15:14:32 -0600

    finalize model building. create non-director model

---
+++ DataTrainandTest/loadDataFrames.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def loadFromCSV(dfName: str) -> pd.DataFrame:
+    df: pd.DataFrame = pd.read_csv(f'~/PycharmProjects/animeClassifier/{dfName}.csv')
+    return df
+
+
+def createFullDFRecursive(fullDF: pd.DataFrame, DataFrames: list, mergePoints: list) -> pd.DataFrame:
+    if len(DataFrames) == 0:
+        return fullDF
+    else:
+        fullDF = pd.merge(fullDF, DataFrames[0], how='inner', on=mergePoints[0])
+        return createFullDFRecursive(fullDF, DataFrames[1:], mergePoints[1:])
+
+
+def createFullDF(baseDF: pd.DataFrame, joins: list, mergePoints: list) -> pd.DataFrame:
+    fullDF: pd.DataFrame = baseDF
+    for iterator, df in enumerate(joins):
+        fullDF = pd.merge(fullDF, df, how='inner', on=mergePoints[iterator])
+    return fullDF.dropna()
---
+++ DataTrainandTest/modelBuildNoDirector.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+from loadDataFrames import loadFromCSV, createFullDF
+
+
+animeDF: pd.DataFrame = loadFromCSV('animeDF')
+formatDF: pd.DataFrame = loadFromCSV('formatDF')
+genreDF: pd.DataFrame = loadFromCSV('genreDF')
+
+DataFrames = [formatDF, genreDF]
+mergePoints = ['anime_id', 'anime_id']
+fullDF = createFullDF(animeDF, DataFrames, mergePoints)
+fullDF.to_csv('~/PycharmProjects/animeClassifier/fullDF_NoDirector.csv', index=False)
+
+featureColumns = ['year_released', 'episodes', 'a_mean_score', 'is_Action', 'is_Adventure', 'is_Comedy', 'is_Drama',
+                  'is_Ecchi', 'is_Sci-Fi', 'is_Fantasy', 'is_Horror', 'is_Mahou_Shoujo', 'is_Mecha', 'is_Music',
+                  'is_Mystery', 'is_Psychological', 'is_Romance', 'is_Slice of Life', 'is_Sports', 'is_Supernatural',
+                  'is_Thriller', 'TV', 'TV_SHORT', 'MOVIE', 'SPECIAL', 'OVA', 'ONA', 'MUSIC']
+X = fullDF[featureColumns]
+y = fullDF['a_do_I_like']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=16)
+logreg = LogisticRegression(random_state=16, max_iter=500)
+logreg.fit(X_train, y_train)
+
+y_prediction = logreg.predict(X_test)
+
+print(X_train)
